ee63436be210adfd5e6c4f3d5d3efc1f56d7e49c
Jon Cuthbert <jon@cuth.net>
2014-03-26 16:03:05 -0400

    add requestAnimationFrame and add placeholder to set frame position fixed

--- example/scripts/main.js
+++ example/scripts/main.js
@@ -1,1 +1,4 @@
-var banners = new ParallaxBanners('.banner','img', { allowReverse: true, offMediaQuery: '(max-width: 500px)' });
+var banners = new ParallaxBanners('.banner', 'img', {
+    allowReverse: true,
+    offMediaQuery: '(max-width: 500px)'
+});
